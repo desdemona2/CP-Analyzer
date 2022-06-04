0b075f431776197370cd5b09c587923074476200
Bhavuk <bhavuksharma2202@gmail.com>
2022-06-04 14:06:53 +0530

    Added adapter for PractiseMode

--- app/src/main/java/com/redheadhammer/cpanalyzer/PractiseMode.java
+++ app/src/main/java/com/redheadhammer/cpanalyzer/PractiseMode.java
@@ -1,6 +1,7 @@
 package com.redheadhammer.cpanalyzer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -8,11 +9,14 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageView;
 
 public class PractiseMode extends AppCompatActivity {
 
     private Chronometer chronometer;
     private RecyclerView recyclerView;
+    private PractiseAdapter practiseAdapter;
+    private ImageView container;
     private boolean isRunning = false;
     private Button button;
     private long offset = 0;
@@ -24,15 +28,28 @@ public class PractiseMode extends AppCompatActivity {
 
         chronometer = findViewById(R.id.chronometer);
         recyclerView = findViewById(R.id.recycler);
+        container = findViewById(R.id.container);
         button = findViewById(R.id.toggle);
 
         /* adding bottom bar functions */
+        // noinspection unused
         BottomBar bottomBar = new BottomBar(PractiseMode.this);
+
+        /* creating practise adapter */
+        practiseAdapter = new PractiseAdapter(PractiseMode.this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(PractiseMode.this));
+        recyclerView.setAdapter(practiseAdapter);
 
         button.setOnClickListener(this::startChron);
         button.setOnLongClickListener(this::onLongClick);
+        container.setOnClickListener(this::onClickChron);
     }
+    public void onClickChron(View view){
+
+    }
+
     public void startChron(View view) {
+
         if (isRunning){
             offset = SystemClock.elapsedRealtime() - chronometer.getBase();
             chronometer.stop();
